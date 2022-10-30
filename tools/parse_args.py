14174b60348381b2052b6970cea2b588e4de4439
erngcho <eulrang.cho@gmail.com>
2022-10-31 04:43:13 +0900

    argparsers modified to be compatible

--- tools/parse_args.py
+++ tools/parse_args.py
@@ -5,15 +5,18 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # task 1
-    parser.add_argument('--clue-path', default=None, help='clue(img, txt) path')  # TODO: img -> hint 수정함 # 경로지정 필수
-    parser.add_argument('--output-path', default='.', help='output path')
-    parser.add_argument('--debug-path', default='.', help='debugging output path')
-    parser.add_argument('--frame-skip', type=int, default=30, help='output path')
-    parser.add_argument('--img-conf-th', type=int, default=0.2,
+    parser.add_argument('--clue_path', default="/home/eulrang/workspace/git/AGC2022_round3_task1/data_toy/drone_task1_toy/case3", help='clue(img, txt) path')
+    parser.add_argument('--output_path', default='/home/eulrang/workspace/git/AGC2022_round3_task1/data_toy/outputs/result.json', help='output path')
+    parser.add_argument('--task1_debug', action="store_true", help='(optional)debug mode')
+    parser.add_argument('--debug_path', default=None, help='debugging output path')
+    parser.add_argument('--yolo_path', default='/home/eulrang/workspace/git/Drone_Challenge/task1/yolov7/models/best.pt', help='yolo task1 checkpoint path')
+    parser.add_argument('--img_conf_th', type=int, default=0.2,
                         help='img threshold')  # TODO: determine best confidence threshold value
-    parser.add_argument('--img-kp-th', type=int, default=5,
+    parser.add_argument('--img_kp_th', type=int, default=5,
                         help='img threshold')  # TODO: determine best keypoint threshold value
-    parser.add_argument('--txt-th', type=int, default=0.8, help='txt threshold')  # for Yolo FIXME
+    parser.add_argument('--txt_th', type=int, default=0.8, help='txt threshold')        # TODO: determine best value
+    parser.add_argument('--total_th', type=int, default=0.9, help='img+txt threshold')  # TODO: determine best value
+
     # task 2
     parser.add_argument('--yolo-weights', type=str, help='model.pt path(s)')
     parser.add_argument('--strong-sort-weights', type=str)
@@ -28,7 +31,7 @@ def parse_args():
     parser.add_argument('--half', action='store_true', default=False, help='use FP16 half-precision inference')
 
     # for debugging
-    parser.add_argument('--video-path', type=str, default=None, help='video path')
+    parser.add_argument('--video_path', type=str, default=None, help='video path')
 
     args = parser.parse_args()
     args.imgsz *= 2 if len(args.imgsz) == 1 else 1  # expand
