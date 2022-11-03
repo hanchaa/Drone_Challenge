c5425d7e1c6ce850678c51c5cd02fc994b49a4fb
JuHan <hanchaa@gmail.com>
2022-11-04 02:28:57 +0900

    change task2 arguments

--- src/run.sh
+++ src/run.sh
@@ -5,9 +5,8 @@ source /opt/ros/melodic/setup.bash
 python main.py \
 --yolo_path task1/weights/yolo.pt \
 --clue_path dataset \
---yolo_weights task2_vision/yolov7/params/10class_800/best.pt \
 --config_strongsort task2_vision/strong_sort/configs/strong_sort.yaml \
---strong_sort_weights task2_vision/osnet_x0_25_msmt17.pt \
---show_vid \
---conf_thres 0.60 \
+--strong_sort_weights task2_vision/weights/osnet_x0_25_msmt17.pt \
+--yolo_weights task2_vision/weights/yolov7_drone_dummy.pt \
+--video_path task2_vision/yolov7/video/set03_drone03.mp4 \
 --device 0
