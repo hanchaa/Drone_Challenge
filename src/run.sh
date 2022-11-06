ad785f3c0049c2433f3c9e94671243f78a9e0504
JuHan <hanchaa@naver.com>
2022-11-06 19:07:34 +0900

    updates

--- src/run.sh
+++ src/run.sh
@@ -11,4 +11,4 @@ python main.py \
 --strong_sort_weights task2_vision/weights/osnet_x0_25_msmt17.pt \
 --yolo_weights task2_vision/weights/task2_safe.pt \
 --checkpoint ./task2_audio/ckpts/2022_11_03_17_47_47.0100.pt \
---max_confidence 0.6
+--max_confidence 0.3
